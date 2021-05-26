1b3e3619766a586f8a007285d9419fbad424c6aa
Kulana Avinash <kulanaavinash2018@gmail.com>
2021-05-26 17:45:30 +0530

    Splash form

--- Mail_Sender_Desktop_Application/Form1.Designer.cs
+++ Mail_Sender_Desktop_Application/Form1.Designer.cs
@@ -106,7 +106,7 @@ namespace Mail_Sender_Desktop_Application
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
-            this.label3.Location = new System.Drawing.Point(120, 406);
+            this.label3.Location = new System.Drawing.Point(125, 406);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(33, 16);
             this.label3.TabIndex = 5;
